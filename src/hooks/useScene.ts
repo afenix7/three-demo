f977716bf42d4507269776cf951ea6b49fce2c88
afenix7 <aphx1988@qq.com>
2023-06-04 17:03:25 +0800

    传输代码到台式机用

--- src/hooks/useScene.ts
+++ src/hooks/useScene.ts
@@ -1,15 +1,22 @@
 import { Height } from './../../node_modules/csstype/index.d'
 import { reactive, watch } from 'vue'
-import { Scene, WebGLRenderer, PerspectiveCamera } from 'three'
+import { Scene, WebGLRenderer, PerspectiveCamera, Color } from 'three'
 
-export const useScene = (config) => {
+interface Config {
+  fov?: number
+  width?: number
+  height?: number
+}
+
+export const useScene = (selector?: string, config?: Config) => {
+  const el = document.querySelector(selector!) || document.body
+  const width = config!.width || el.clientWidth || window.innerWidth
+  const height = config!.height || el.clientHeight || window.innerHeight
   const scene = reactive(new Scene())
   const renderer = reactive(new WebGLRenderer({ antialias: true }))
-  const camera = reactive(
-    new PerspectiveCamera(
-      config.fov || 75,
-      config.width || window.innerWidth,
-      config.height || window.innerHeight
-    )
-  )
+  const camera = reactive(new PerspectiveCamera(config!.fov || 75))
+  renderer.setClearColor(new Color(0, 0, 255))
+  renderer.setSize(width, height)
+  el.appendChild(renderer.domElement)
+  return [scene, renderer, camera]
 }
